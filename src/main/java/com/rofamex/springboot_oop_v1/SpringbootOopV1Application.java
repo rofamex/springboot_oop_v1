58c4acf4b1ee796c2e1331ba52aa8514b7a5ecc4
Robson Lee <rofamex@gmail.com>
2021-04-08 16:28:28 -0400

    Initializing

---
+++ src/main/java/com/rofamex/springboot_oop_v1/SpringbootOopV1Application.java
@@ -0,0 +1,13 @@
+package com.rofamex.springboot_oop_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOopV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOopV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rofamex/springboot_oop_v1/SpringbootOopV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rofamex.springboot_oop_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOopV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
